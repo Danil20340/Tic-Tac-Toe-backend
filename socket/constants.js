f9bb3eab5672a4a096044d47899390703d4099b1
Даниил <bronnikovds@gmail.com>
2025-01-28 21:10:44 +0500

    delete unused code

--- socket/constants.js
+++ socket/constants.js
@@ -1,12 +1,6 @@
-const PlayerStatuses = {
-    AVAILABLE: 'AVAILABLE',
-    IN_GAME: 'IN_GAME',
-    DISCONNECTED: 'DISCONNECTED',
-};
-
 const TIMEOUTS = {
     INVITE_RESPONSE: 10_000, // 10 секунд
     DISCONNECT_GRACE_PERIOD: 300_000, // 5 минут
 };
 
-module.exports = { PlayerStatuses, TIMEOUTS };
+module.exports = { TIMEOUTS };
--- socket/state.js
+++ socket/state.js
@@ -1,11 +1,9 @@
 const onlinePlayers = new Map();
-const playerStates = new Map();
 const disconnectTimers = new Map();
 const pendingInvites = new Map();
 
 module.exports = {
     onlinePlayers,
-    playerStates,
     disconnectTimers,
     pendingInvites
 };
