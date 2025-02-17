7c3d708f865d21eb0801bbabca56ea1a71f127bf
Даниил <bronnikovds@gmail.com>
2025-02-17 21:42:28 +0500

    delete logs

--- socket/handlers/disconnect.js
+++ socket/handlers/disconnect.js
@@ -8,7 +8,6 @@ module.exports = (socket, io) => {
         const playerId = socket.data.playerId;
 
         if (playerId) {
-            console.log(`Игрок ${playerId} отключился.`);
 
             // Удаляем игрока из onlinePlayers
             onlinePlayers.delete(playerId);
@@ -20,26 +19,14 @@ module.exports = (socket, io) => {
                 })
                 .then((player) => {
                     if (player && player.availability === 'IN_GAME') {
-                        console.log(
-                            `Игрок ${playerId} найден в базе данных со статусом 'IN_GAME'. Устанавливаем таймер на 5 минут.`
-                        );
-
                         // Устанавливаем таймер на 5 минут (300000 мс)
                         const timerId = setTimeout(async () => {
-                            console.log(
-                                `Игрок ${playerId} не вернулся за 5 минут. Меняем статус на 'AVAILABLE'.`
-                            );
-
                             try {
                                 // Обновляем статус игрока в базе данных
                                 await prisma.player.update({
                                     where: { id: playerId },
                                     data: { availability: 'AVAILABLE' },
                                 });
-
-                                console.log(
-                                    `Статус игрока ${playerId} успешно обновлён в базе данных на 'AVAILABLE'.`
-                                );
                             } catch (error) {
                                 console.error(
                                     `Ошибка при обновлении статуса игрока ${playerId} в базе данных:`,
@@ -57,9 +44,6 @@ module.exports = (socket, io) => {
                         // Сохраняем таймер в disconnectTimers
                         disconnectTimers.set(playerId, timerId);
                     } else {
-                        console.log(
-                            `Игрок ${playerId} не найден или имеет статус, отличный от 'IN_GAME'.`
-                        );
                     }
                 })
                 .catch((error) => {
