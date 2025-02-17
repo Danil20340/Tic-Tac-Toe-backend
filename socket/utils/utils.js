1379f49620aa969622ed9e3cac98a47f969889cc
Даниил <bronnikovds@gmail.com>
2025-02-17 21:41:28 +0500

    new utils:updatePlayerStatusInMemory and updateMoveTimer

--- socket/utils/utils.js
+++ socket/utils/utils.js
@@ -1,4 +1,5 @@
-const { onlinePlayers } = require('../state');
+const { onlinePlayers, moveTimers } = require('../state');
+const { prisma } = require("../../prisma/prisma-client");
 const updateOnlinePlayers = (io) => {
     const onlinePlayersList = Array.from(onlinePlayers.entries()).map(([id, player]) => ({
         id,
@@ -7,5 +8,57 @@ const updateOnlinePlayers = (io) => {
     }));
     io.emit('online-players', onlinePlayersList);
 };
+function updatePlayerStatusInMemory(playerId, availability) {
+    if (onlinePlayers.has(playerId)) {
+        const player = onlinePlayers.get(playerId);
+        onlinePlayers.set(playerId, {
+            ...player,
+            availability,
+        });
+    }
+}
+//Создаем таймер хода
+const updateMoveTimer = async (io, gameId) => {
+    // Таймер для предупреждения за минуту до окончания
+    const alertTimeout = setTimeout(() => {
+        io.to(gameId).emit('alertAboutMove');
+    }, 4 * 60 * 1000); // 4 минуты
 
-module.exports = { updateOnlinePlayers };
+    // Основной таймер
+    const mainTimeout = setTimeout(async () => {
+        try {
+            const game = await prisma.game.findUnique({ where: { id: gameId } });
+
+            if (!game) return;
+
+            // Определяем проигравшего
+            const loserId = game.nowMove === game.player1Symbol ? game.player1Id : game.player2Id;
+            const winnerId = loserId === game.player1Id ? game.player2Id : game.player1Id;
+
+            // Обновляем статус игры и назначаем победителя
+            const updatedGame = await prisma.game.update({
+                where: { id: gameId },
+                data: {
+                    status: "FINISHED",
+                    winnerId: winnerId,
+                    endTime: new Date()
+                }
+            });
+            io.to(gameId).emit('moveMade', { ...updatedGame });
+            //Обновляем статусы доступности игроков
+            await prisma.player.updateMany({
+                where: { id: { in: [game.player1Id, game.player2Id] } },
+                data: { availability: 'AVAILABLE' }
+            });
+            updatePlayerStatusInMemory(game.player1Id, 'AVAILABLE');
+            updatePlayerStatusInMemory(game.player2Id, 'AVAILABLE');
+            io.socketsLeave(gameId);
+            
+        } catch (error) {
+            console.error("Ошибка при обработке таймера хода:", error);
+        }
+    }, 5 * 60 * 1000); // 5 минут
+
+    moveTimers.set(gameId, { alertTimeout, mainTimeout });
+};
+module.exports = { updateOnlinePlayers, updatePlayerStatusInMemory, updateMoveTimer };
