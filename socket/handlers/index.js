9e9791e96ac8417a0d6c731024b393e7322124a9
Даниил <bronnikovds@gmail.com>
2025-02-17 21:40:57 +0500

    new hadlers

--- socket/handlers/index.js
+++ socket/handlers/index.js
@@ -1,11 +1,15 @@
-const disconnectHandler = require('./disconnect');
 const connectionHandler = require('./connection');
+const disconnectHandler = require('./disconnect');
 const inviteHandler = require('./invite');
 const getPlayersHandler = require('./initOnlinePlayers');
+const getChat = require('./chat');
+const gameMove = require('./gameMove');
 
 module.exports = (socket, io) => {
-    inviteHandler(socket, io);
-    disconnectHandler(socket, io);
     connectionHandler(socket, io);
+    disconnectHandler(socket, io);
+    inviteHandler(socket, io);
     getPlayersHandler(socket, io);
+    getChat(socket, io);
+    gameMove(socket, io);
 };
