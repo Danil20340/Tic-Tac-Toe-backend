eec0572e3e936b7fa8eebb77ade659a8039984e1
Даниил <bronnikovds@gmail.com>
2025-02-17 21:39:59 +0500

    new states

--- socket/state.js
+++ socket/state.js
@@ -1,9 +1,13 @@
 const onlinePlayers = new Map();
 const disconnectTimers = new Map();
 const pendingInvites = new Map();
+const moveTimers = new Map();
+const chatBuffer = {};
 
 module.exports = {
     onlinePlayers,
     disconnectTimers,
-    pendingInvites
+    pendingInvites,
+    chatBuffer,
+    moveTimers
 };
